edf14f64bd6d4299143d72aa49a8a33c9c145b77
Tiago Martins <tiago_martins238@hotmail.com>
2023-05-18 21:20:20 -0300

    create jwt token

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -53,8 +53,17 @@ export async function authRoutes(app: FastifyInstance) {
         },
       })
     }
+
+    const token = app.jwt.sign({
+      name: user.name,
+      avatarUrl: user.avatarUrl,
+    },{
+      sub: user.id,
+      expiresIn: '30 days',
+    })
+
     return {
-      user,
+      token,
     }
   })
 }
