e4aac167c11b6fa48d3e64103bc5eb5015794929
Tiago Martins <tiago_martins238@hotmail.com>
2023-05-19 23:23:45 -0300

    create route logout

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,7 @@ app.register(memoriesRoutes)
 app
   .listen({
     port: 3333,
-    host: '0.0.0.0',
+    host: '::',
   })
   .then(() => {
     console.log('http server running on http://localhost:3333')
